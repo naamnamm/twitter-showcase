4e6e50db7d52757d9fba396d58c698f9390839fc
Naam <naam.pt@gmail.com>
2020-07-29 08:27:26 -0400

    update

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
-console.log('app ran')
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.use(express.static('src'));
+
+//define endpoint
+app.get('/', (req, res) => res.send('Hello World!'))
+
+//start the application
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))

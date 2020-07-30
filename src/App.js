fd44166da4ddd8dfdcdf449410998d80158f3cf2
Naam <naam.pt@gmail.com>
2020-07-30 10:50:22 -0400

    update navbar

--- src/App.js
+++ src/App.js
@@ -4,23 +4,28 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import HomePage from './components/HomePage'
 import RandomTweetPage from './components/RandomTweetPage'
 import SearchTweetPage from './components/SearchTweetPage'
-import NavBar from './components/NavBar'
+import NavigationBar from './components/NavigationBar'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 const App = () => {
 
 
   return (
-    <Router>
-      <div className="App">
-        <NavBar />
-        <Switch>
-          <Route path='/' exact component={HomePage} />
-          <Route path='/search' component={SearchTweetPage} />
-          <Route path='/random' component={RandomTweetPage} />
-        </Switch>
-      </div>
-    </Router>
+    <>
+
+      <Router>
+        <NavigationBar />
+
+        <div className="App">
+          <Switch>
+            <Route path='/' exact component={HomePage} />
+            <Route path='/search' component={SearchTweetPage} />
+            <Route path='/random' component={RandomTweetPage} />
+          </Switch>
+        </div>
+
+      </Router>
+    </>
   );
 }
 

4a750d457f66eca1b619e569efee5a829a7139dc
Naam <naam.pt@gmail.com>
2020-08-07 12:02:49 -0400

    install node-fetch

--- server.js
+++ server.js
@@ -2,16 +2,20 @@ const express = require('express');
 
 const app = express();
 
-app.get('/api/customers', (req, res) => {
-  const customers = [
-    { id: 1, name: 'John', age: 8 },
-    { id: 2, name: 'Mary', age: 9 },
-    { id: 3, name: 'Henry', age: 10 },
-  ];
-
-  res.json(customers);
-});
-
 const port = 5000;
 
+app.get('https://api.twitter.com/1.1/search/tweets.json?q=nasa', (req, res) => {
+  console.log(req);
+});
+
 app.listen(port, () => console.log(`server started on port ${port}`));
+
+// app.get('/api/customers', (req, res) => {
+//   const customers = [
+//     { id: 1, name: 'John', age: 8 },
+//     { id: 2, name: 'Mary', age: 9 },
+//     { id: 3, name: 'Henry', age: 10 },
+//   ];
+
+//   res.json(customers);
+// });
